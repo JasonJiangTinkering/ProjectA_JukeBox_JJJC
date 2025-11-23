a07117fef81dd8d4165a88f9dc15d1614d75a0c2
jcipher-edu <232122381+jcipher-edu@users.noreply.github.com>
2025-11-23 12:49:04 -0500

    Created purchaseQueue class
    Finished Goal 3 - the song queue part of the project.
    Made a purchaseQueue class that uses Deque.
    Added tests to show everything works.
    Had to add a refund method to balanceBox for the refund requirement.
    Created the jar file too.

---
+++ GoalSet3/paymentLauncher.java
@@ -0,0 +1,11 @@
+interface paymentLauncher {
+    public static void main(String[] args){
+        System.out.println("GoalSet2 Main function started");
+    }
+
+    public int takePayment(); 
+
+
+    public String returnFunds();
+   
+}

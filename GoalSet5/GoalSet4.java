d4405de2068cf11a8a093eb759e949585be3ac3f
jcipher-edu <232122381+jcipher-edu@users.noreply.github.com>
2025-12-13 14:31:10 -0500

    Finish Goal 5 JavaFX GUI (testGoal5)
    Added testGoal5 JavaFX interface, wired Enqueue to purchaseQueue, created testGoal5.jar for running the GUI. See README for Goal 5 requirements and run instructions.

---
+++ GoalSet5/GoalSet4.java
@@ -0,0 +1,17 @@
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+import javafx.stage.Stage;
+import javafx.application.Application;
+class GoalSet4{
+    public static void main(String[] args){
+        // System.out.println("Must be run with 1 arg, Song.txt file")
+        
+
+
+        Application.launch(SongPlayer.class, args);
+        
+    }
+
+
+}
